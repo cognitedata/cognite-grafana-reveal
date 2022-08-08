64f62af5efba42b2a57bdf2d398de695cad5063c
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-08-08 13:00:03 +0300

    added variables support

--- src/D3ModelPanel.tsx
+++ src/D3ModelPanel.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { PanelProps } from '@grafana/data';
 import { SingleStatBaseOptions } from '@grafana/ui';
+import { parseInt } from 'lodash';
+import { getTemplateSrv, TemplateSrv } from '@grafana/runtime';
 import { cogniteClient } from './client';
 import { D3ModelOptions } from './types';
 import { startView, fitCameraToAssetId } from './utils';
 
 type Props = PanelProps<SingleStatBaseOptions & D3ModelOptions>;
 
-export const D3ModelPanel: React.FC<Props> = ({
-  options: { selectedDatasource, selectedProject, selected3DModel, zoomToAssetId },
-  width,
-  height,
-}) => {
+export const D3ModelPanel: React.FC<Props> = (props) => {
+  const {
+    options: { selectedDatasource, selectedProject, selected3DModel, zoomToAssetId },
+    width,
+    height,
+    replaceVariables,
+  } = props;
   const ref = useRef<HTMLInputElement>(null);
   const [viewer, setViewer] = useState(null);
   const [model, setModel] = useState(null);
+  const templateSrv: TemplateSrv = getTemplateSrv();
+  const variablesOptions = templateSrv.getVariables();
   const client = useMemo(() => {
     return (
       selectedDatasource?.baseUrl &&
@@ -22,7 +28,13 @@ export const D3ModelPanel: React.FC<Props> = ({
       cogniteClient(selectedProject.id, selectedDatasource?.baseUrl)
     );
   }, [selectedDatasource, selectedProject]);
-
+  const zoomInTo = (zoomToAssetId, model, viewer, client) =>
+    fitCameraToAssetId(
+      typeof zoomToAssetId === 'string' ? parseInt(replaceVariables(zoomToAssetId)) : zoomToAssetId,
+      model,
+      viewer,
+      client
+    );
   const display = async (client, viewer) => {
     const model = await viewer.addModel(selected3DModel);
     setModel(model);
@@ -34,7 +46,7 @@ export const D3ModelPanel: React.FC<Props> = ({
       }
     });
     if (zoomToAssetId) {
-      fitCameraToAssetId(zoomToAssetId, model, viewer, client);
+      zoomInTo(zoomToAssetId, model, viewer, client);
     }
   };
   useEffect(() => {
@@ -49,9 +61,9 @@ export const D3ModelPanel: React.FC<Props> = ({
   }, [selectedProject, selected3DModel, client]);
   useEffect(() => {
     if (zoomToAssetId && model) {
-      fitCameraToAssetId(zoomToAssetId, model, viewer, client);
+      zoomInTo(zoomToAssetId, model, viewer, client);
     }
-  }, [zoomToAssetId]);
+  }, [zoomToAssetId, variablesOptions]);
   return (
     <div style={{ width, height }} ref={ref}>
       Select a 3D Model
